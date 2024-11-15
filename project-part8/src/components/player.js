bdb6a8dc60c0ed95194e57f8c55ceb531434362e
Cade Phillips <cadeap@email.sc.edu>
2024-11-15 15:23:57 -0500

    updated links

--- project-part8/src/components/player.js
+++ project-part8/src/components/player.js
@@ -4,7 +4,7 @@ import "../css/player.css"
 const Player = (props) => {
     return (
         <section id="player">
-            <img src={"http://localhost:3002/" + props.image} alt={props.name} />
+            <img src={"https://project-part9.onrender.com/" + props.image} alt={props.name} />
             <p>Name: {props.name} </p>
             <p>Number: {props.number}</p>
             <p>Positions: {props.position}</p>
